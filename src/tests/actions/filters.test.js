285e7399ebf38f60ee918dc845590af2b6bac6e7
paulcmd <choxmail.pm@gmail.com>
2020-12-16 03:24:50 -0700

    created start date test

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -2,6 +2,12 @@ import { setStartDate, setEndDate } from '../../actions/filters'
 
 test('should generate set start date action object', () => {
 
+    const action = setStartDate(moment(0))
+
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    })
 })
 
 test('should generate set end date action object', () => {
