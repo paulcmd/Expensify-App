fd08138ff6fc389ce950521ea2d35b6e09ba53ac
paulcmd <choxmail.pm@gmail.com>
2021-01-23 16:49:51 -0700

    sort by date test case working

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -18,3 +18,17 @@ test('should set sortBy to amount', () => {
     const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT'})
     expect(state.sortBy).toBe('amount')
 })
+
+test('should set sortBy to date', ()=> {
+    
+    const currentState = {
+        text: '',
+        startDate: undefined,
+        endDate: undefined,
+        sortBy: 'amount'
+    }
+
+    const action = { type: 'SORT_BY_DATE'}
+    const state = filtersReducer(currentState, action)
+    expect(state.sortBy).toBe('date')
+})
