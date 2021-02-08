c4ff96feb6dfea8d10bbcdeec07dbd1494f8e6a4
paulcmd <choxmail.pm@gmail.com>
2021-02-07 18:18:32 -0700

    created separate script to handle production build with -p flag

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const path = require('path')
 
-module.exports = {
-    entry: './src/app.js',
+module.exports = (env) => {
 
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
+    const isProduction = env === 'production'
+     
+    return {
+        entry: './src/app.js',
 
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-
-    devtool: 'cheap-module-eval-source-map',   //takes you to the exact code you wrote when debugging with console, instead of the generated babel code
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
     
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true                //tells browser we are handling all routing through index.html. Routing will be handled client-side, not server-side
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+    
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',   //cheap-module-eval-source-map takes you to the exact code you wrote when debugging with console, instead of the generated babel code
+        
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true                //tells browser we are handling all routing through index.html. Routing will be handled client-side, not server-side
+        }
+
     }
 }
