a3a50644bb9d445a2d23213e85c41ee8a1ab1263
paulcmd <choxmail.pm@gmail.com>
2021-02-09 04:03:03 -0700

    configured express to serve index.html

--- server/server.js
+++ server/server.js
@@ -5,8 +5,13 @@ const app = express()
 const publicPath = path.join(__dirname, '..', 'public')  //go to root, then up one level '..' then serve files in public folder
 
 app.use(express.static(publicPath))
-const PORT = process.env.PORT
+const PORT = 3000
 
-app.listen(3000, () => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+// * will serve all routes not in the public folder
+
+app.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`)
 })
