71abf5d033d212622967eb63831dfee485e907b2
paulcmd <choxmail.pm@gmail.com>
2021-02-14 01:25:52 -0700

    created app on heroku

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '..', 'public')  //go to root, then up one level '..' then serve files in public folder
 
 app.use(express.static(publicPath))
-const PORT = 3000
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
-// * will serve all routes not in the public folder
+// * will serve all routes not in the public 
+//10 - if what user requested is not in public folder, just serve index.html
 
 app.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`)
