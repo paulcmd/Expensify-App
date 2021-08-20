5747d82477deeeddc5cc98646e198a4f0ed4c6db
Paul <choxmail.pm@gmail.com>
2021-08-19 23:55:00 -0700

    added redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import expensesReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
 import thunk from 'redux-thunk'
@@ -11,7 +11,8 @@ export default () => {
             expenses: expensesReducer,
             filters: filtersReducer
         }),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
+        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
     return store
 }
