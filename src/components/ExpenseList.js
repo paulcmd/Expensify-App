c842d4356c841873bcd08085120d9978f97d2291
Paul <choxmail.pm@gmail.com>
2021-09-06 23:56:26 -0700

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
 import filteredExpenses from '../selectors/expenses'
 
-//exported defaults can be given any name eg. filteredExpense
+//exported defaults can be given any name eg. filteredExpenses
 const ExpenseList = ({ expenses }) => (
     <div>
         <h1>Expense List</h1>
