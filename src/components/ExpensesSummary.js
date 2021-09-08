494a3a0cd8a5984df324b9df04aed7e3297e3777
Paul <choxmail.pm@gmail.com>
2021-09-08 15:55:53 -0700

    Update ExpensesSummary.js

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,16 +4,25 @@ import numeral from 'numeral'
 import selectExpenses from '../selectors/expenses'
 import selectExpensesTotal from '../selectors/expenses-total'
 
-export const ExpensesSummary = () => {
+export const ExpensesSummary = ({ expenseCount, expenseTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
+    const formattedExpenseTotal = numeral(expenseTotal / 100).format('$0,0.00')
 
-
+    return (
+        <div>
+            <h1>
+                Viewing {expenseCount} {''}
+                {expenseWord} totalling {formattedExpenseTotal}
+            </h1>
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => {
-	const visibleExpenses = selectExpenses(state.expenses, state.filters)
-//visibleExpenses will give us expenses within the range we selected on the calendar
-	return {
-		expenseCount: visibleExpenses.length,
-		expenseTotal : selectExpensesTotal(visibleExpenses)
-	}
+    const visibleExpenses = selectExpenses(state.expenses, state.filters)
+    //visibleExpenses will give us expenses within the range we selected on the calendar
+    return {
+        expenseCount: visibleExpenses.length,
+        expenseTotal: selectExpensesTotal(visibleExpenses)
+    }
 }
