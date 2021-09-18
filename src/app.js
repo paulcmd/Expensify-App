4136bdaf68c155418c7ca3e3ac8af9848f7ddd94
Paul <choxmail.pm@gmail.com>
2021-09-17 23:34:59 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ firebase.auth().onAuthStateChanged((user) => {
             history.push('/dashboard')
         }
         
-        console.log('User is logged in!')
+        console.log('User is logged!')
     } else {
         renderApp()
         history.push('/')
