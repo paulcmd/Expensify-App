84213b072ccab650ee755d44b45407716ec98120
Paul <choxmail.pm@gmail.com>
2021-09-27 00:04:56 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import { login, logout } from './reducers/auth'
 import { startSetExpenses } from './actions/expenses'
 import { firebase } from './firebase/firebase'
 
